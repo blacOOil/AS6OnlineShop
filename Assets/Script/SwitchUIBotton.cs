50ef5dbc8650cc70c11a0ab3ffca155232e1adf3
Gunjaa1 <gunjaa1@hotmail.com>
2023-10-06 18:30:12 +0700

    Add : CloseShopButton2

--- Assets/Script/SwitchUIBotton.cs
+++ Assets/Script/SwitchUIBotton.cs
@@ -13,26 +13,22 @@ public class SwitchUIBotton : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        // if (target.value == 0f) {
+        //     CanvasUISwitchNew();
+        // } else if (target.value == 1f) {
+        //     CanvasUISwitchOld();
+        // }
+    }
+
+    public void CanvasUISwitch() {
         if (target.value == 0f) {
-            CanvasUISwitchNew();
-        } else if (target.value == 1f) {
-            CanvasUISwitchOld();
+            NewCanvasUI.SetActive(true);
+            OldCanvasUI.SetActive(false);
+            NewCanvasUiActive = true;
+        } else {
+            NewCanvasUI.SetActive(false);
+            OldCanvasUI.SetActive(true);
+            NewCanvasUiActive = false;
         }
-    }
-
-    public void CanvasUISwitchNew() {
-        NewCanvasUI.SetActive(true);
-        OldCanvasUI.SetActive(false);
-        NewCanvasUiActive = true;
-        //Debug.Log("ItemInfo Pop-up");
-    }
-
-    public void CanvasUISwitchOld() {
-        NewCanvasUI.SetActive(false);
-        OldCanvasUI.SetActive(true);
-        NewCanvasUiActive = false;
-        Debug.Log("ItemInfo Pop-down");
-    }
-
-    
+    }  
 }
